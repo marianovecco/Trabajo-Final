c600099bb4c9565377dba662aceafb47d7cff643
marianovecco <31785478+marianovecco@users.noreply.github.com>
2017-12-12 00:33:42 -0300

    Implementacion de fixtures en los tests
    Tambien se testearon:
    -Obtencion de los movimientos de una categoria y un usuario
    -Obtencion de los usuarios y las categorias de una cuenta
    -Borrado logico de un usuario en caso de tener movimientos y fisico en caso de no tenerlos

--- tpfinal/controlgastos/tests.py
+++ tpfinal/controlgastos/tests.py
@@ -8,6 +8,7 @@ from rest_framework.renderers import JSONRenderer
 import json
 
 class CategoriaTests(APITestCase):
+    fixtures = ['db.json']
     def test_listar_categorias(self):
         """
         Test de listado de caterogias
@@ -15,25 +16,21 @@ class CategoriaTests(APITestCase):
         url = '/controlgastos/categorias'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json.loads(response.content),[]) #Por ahora dejemoslo asi, despues veo si usamos fixtures y hay que cambiarlo
-
+        self.assertEqual(json.loads(response.content),[{'id': 1, 'nombre': 'Compras', 'cuenta': 1}])
 
     def test_crear_categoria(self):
         """
         Test de creacion de una categoria
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
         url = '/controlgastos/categorias'
-        response = self.client.post(url, {'nombre': 'Compras','cuenta':'1'}, format='json')
+        response = self.client.post(url, {'nombre': 'Supermercado','cuenta':'1'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(json.loads(response.content),{'id': 1, 'nombre': 'Compras', 'cuenta': 1})
+        self.assertEqual(json.loads(response.content),{'id': 2, 'nombre': 'Supermercado', 'cuenta': 1})
 
     def test_obtener_categoria(self):
         """
         Test de obtencion de una sola categoria
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/categorias', {'nombre': 'Compras','cuenta':'1'}, format='json')
         url = '/controlgastos/categorias/1'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -43,11 +40,18 @@ class CategoriaTests(APITestCase):
         """
         Test de borrado de una sola categoria
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/categorias', {'nombre': 'Compras','cuenta':'1'}, format='json')
         url = '/controlgastos/categorias/1'
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_obtener_movimientos(self):
+        """
+        Test de obtencion de los movimientos de una categoria
+        """
+        url = '/controlgastos/categorias/1/movimientos'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),[{'id': 1, 'fecha': '2017-07-12','usuario':1,'monto':2000,'categoria':1,'descripcion':'pepe'}])
 
     def test_bad_request(self):
         """
@@ -61,13 +65,12 @@ class CategoriaTests(APITestCase):
         """
         Testea cuando se pide una categoria que no existe
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/categorias', {'nombre': 'Compras','cuenta':'1'}, format='json')
         url = '/controlgastos/categorias/20'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 class UsuarioTests(APITestCase):
+    fixtures = ['db.json']
     def test_listar_usuarios(self):
         """
         Test de listado de usuarios
@@ -75,38 +78,50 @@ class UsuarioTests(APITestCase):
         url = '/controlgastos/usuarios'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json.loads(response.content),[])
+        self.assertEqual(json.loads(response.content),[{'id': 1, 'user': 'Pepito','password': '1234','email':'hola@123.com', 'cuenta': 1},{'id': 2, 'user': 'Pepon','password': '123478','email':'pepon@123.com', 'cuenta': 1}])
 
     def test_crear_usuario(self):
         """
         Test de creacion de una usuario
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
         url = '/controlgastos/usuarios'
         response = self.client.post(url, {'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(json.loads(response.content),{'id': 1, 'user': 'user1','password': 'contraseña','email':'user1@gmail.com', 'cuenta': 1})
+        self.assertEqual(json.loads(response.content),{'id': 4, 'user': 'user1','password': 'contraseña','email':'user1@gmail.com', 'cuenta': 1})
 
     def test_obtener_usuario(self):
         """
         Test de obtencion de un solo usuario
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/usuarios', {'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
         url = '/controlgastos/usuarios/1'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json.loads(response.content),{'id': 1, 'user': 'user1','password': 'contraseña','email':'user1@gmail.com', 'cuenta': 1})
+        self.assertEqual(json.loads(response.content),{'id': 1, "user": "Pepito", "password": "1234", "email": "hola@123.com", "cuenta": 1})
 
-    def test_borrar_usuario(self):
+    def test_borrar_usuario_con(self):
         """
-        Test de borrado de un solo usuario
+        Test de borrado de un solo usuario con movimientos
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/usuarios', {'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
         url = '/controlgastos/usuarios/1'
         response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_borrar_usuario_sin(self):
+        """
+        Test de borrado de un solo usuario sin movimientos
+        """
+        url = '/controlgastos/usuarios/2'
+        response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_obtener_movimientos(self):
+        """
+        Test de obtencion de los movimientos de un usuario
+        """
+        url = '/controlgastos/usuarios/1/movimientos'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),[{'id': 1, 'fecha': '2017-07-12','usuario':1,'monto':2000,'categoria':1,'descripcion':'pepe'}])
 
     def test_bad_request(self):
         """
@@ -120,13 +135,12 @@ class UsuarioTests(APITestCase):
         """
         Testea cuando se pide un usuario que no existe
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/usuarios', {'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
         url = '/controlgastos/usuarios/20'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 class CuentaTests(APITestCase):
+    fixtures = ['db.json']
     def test_listar_cuentas(self):
         """
         Test de listado de cuentas
@@ -134,36 +148,52 @@ class CuentaTests(APITestCase):
         url = '/controlgastos/cuentas'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json.loads(response.content),[])
+        self.assertEqual(json.loads(response.content),[{'id': 1, 'nombre': 'Cuenta1'}])
 
     def test_crear_cuenta(self):
         """
         Test de creacion de una cuenta
         """
         url = '/controlgastos/cuentas'
-        response = self.client.post(url, {'nombre': 'cuenta1'}, format='json')
+        response = self.client.post(url, {'nombre': 'Cuenta2'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(json.loads(response.content),{'id': 1, 'nombre': 'cuenta1'})
+        self.assertEqual(json.loads(response.content),{'id': 2, 'nombre': 'Cuenta2'})
 
     def test_obtener_cuenta(self):
         """
         Test de obtencion de una cuenta
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
         url = '/controlgastos/cuentas/1'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json.loads(response.content),{'id': 1, 'nombre': 'cuenta1'})
-
+        self.assertEqual(json.loads(response.content),{'id': 1, 'nombre': 'Cuenta1'})
 
     def test_borrar_cuenta(self):
         """
         Test de borrado de una cuenta
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
         url = '/controlgastos/cuentas/1'
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_obtener_categorias(self):
+        """
+        Test de obtencion de las caterogias de una cuenta
+        """
+        url = '/controlgastos/cuentas/1/categorias'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),[{'id': 1, 'nombre': 'Compras', 'cuenta': 1}])
+
+    def test_obtener_usuarios(self):
+        """
+        Test de obtencion de los usuarios de una cuenta
+        """
+        url = '/controlgastos/cuentas/1/usuarios'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),[{'id': 1, 'user': 'Pepito','password': '1234','email':'hola@123.com', 'cuenta': 1},{'id': 2, 'user': 'Pepon','password': '123478','email':'pepon@123.com', 'cuenta': 1}])
+
 
     def test_bad_request(self):
         """
@@ -178,7 +208,6 @@ class CuentaTests(APITestCase):
         """
         Testea cuando se pide una cuenta que no existe
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
         url = '/controlgastos/cuentas/20'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
@@ -187,6 +216,7 @@ class CuentaTests(APITestCase):
 
 
 class MovimientoTests(APITestCase):
+    fixtures = ['db.json']
     def test_listar_movimientos(self):
         """
         Test de listado de movimientos
@@ -194,28 +224,21 @@ class MovimientoTests(APITestCase):
         url = '/controlgastos/movimientos'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json.loads(response.content),[])
+        self.assertEqual(json.loads(response.content),[{'id':1, 'fecha':'2017-07-12','usuario':1,'monto':2000,'categoria':1,'descripcion':'pepe'}])
 
     def test_crear_movimiento(self):
         """
         Test de creacion de un movimiento
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/usuarios', {'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
-        self.client.post('/controlgastos/categorias', {'nombre': 'Compras','cuenta':'1'}, format='json')
         url = '/controlgastos/movimientos'
-        response = self.client.post(url, {'fecha':'2017-07-12','usuario':'1','monto':'2000','categoria':'1','descripcion':'pepe'}, format='json')
+        response = self.client.post(url, {'fecha':'2017-07-12','usuario':'1','monto':'3500','categoria':'1','descripcion':'pepito'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(json.loads(response.content),{'id':1, 'fecha':'2017-07-12','usuario':1,'monto':2000,'categoria':1,'descripcion':'pepe'})
+        self.assertEqual(json.loads(response.content),{'id':2, 'fecha':'2017-07-12','usuario':1,'monto':3500,'categoria':1,'descripcion':'pepito'})
 
     def test_obtener_movimiento(self):
         """
         Test de obtencion de un solo movimiento
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/usuarios', {'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
-        self.client.post('/controlgastos/categorias', {'nombre': 'Compras','cuenta':'1'}, format='json')
-        self.client.post('/controlgastos/movimientos', {'fecha':'2017-07-12','usuario':'1','monto':'2000','categoria':'1','descripcion':'pepe'}, format='json')
         url = '/controlgastos/movimientos/1'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -225,10 +248,6 @@ class MovimientoTests(APITestCase):
         """
         Test de borrado de un solo movimiento
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('/controlgastos/usuarios', {'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
-        self.client.post('/controlgastos/categorias', {'nombre': 'Compras','cuenta':'1'}, format='json')
-        self.client.post('/controlgastos/movimientos', {'fecha':'2017-07-12','usuario':'1','monto':'2000','categoria':'1','descripcion':'pepe'}, format='json')
         url = '/controlgastos/movimientos/1'
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
@@ -245,9 +264,6 @@ class MovimientoTests(APITestCase):
         """
         Testea cuando se pide un movimiento que no existe
         """
-        self.client.post('/controlgastos/cuentas', {'nombre': 'cuenta1'}, format='json')
-        self.client.post('controlgastos/usuarios',{'user': 'user1','password': 'contraseña','email':'user1@gmail.com','cuenta':'1'}, format='json')
-        self.client.post('/controlgastos/categorias', {'nombre': 'Compras','cuenta':'1'}, format='json')
         url = '/controlgastos/movimientos/20'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
